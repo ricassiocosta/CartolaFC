b4f3a4110f971df69ae1f87597debb7fc7f0de14
ricassio costa <ricassio_costa@outlook.com>
2022-12-05 22:18:35 -0300

    fixing match struct

--- consolidation/internal/domain/entity/match.go
+++ consolidation/internal/domain/entity/match.go
@@ -25,6 +25,8 @@ type Match struct {
 	ID      string
 	TeamA   *Team
 	TeamB   *Team
+	TeamAID string
+	TeamBID string
 	Date    time.Time
 	Status  string
 	Result  MatchResult
@@ -33,9 +35,11 @@ type Match struct {
 
 func NewMatch(id string, teamA, teamB *Team, date time.Time) *Match {
 	return &Match{
-		ID:    id,
-		TeamA: teamA,
-		TeamB: teamB,
-		Date:  date,
+		ID:      id,
+		TeamA:   teamA,
+		TeamB:   teamB,
+		TeamAID: teamA.ID,
+		TeamBID: teamB.ID,
+		Date:    date,
 	}
 }
